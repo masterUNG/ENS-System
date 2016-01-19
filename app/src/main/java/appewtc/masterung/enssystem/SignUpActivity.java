b033a1a3869ba1e7ca9b2afd4b980d0be53d0683
masterUNG <phrombutr@gmail.com>
2016-01-19 13:46:59 +0700

    Bind Widget

--- app/src/main/java/appewtc/masterung/enssystem/SignUpActivity.java
+++ app/src/main/java/appewtc/masterung/enssystem/SignUpActivity.java
@@ -1,13 +1,46 @@
 package appewtc.masterung.enssystem;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioGroup;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    //Explicit
+    private EditText userEditText, passwordEditText, nameEditText, surnameEditText,
+            phoneEditText, emailEditText;
+    private RadioGroup sexRadioGroup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-    }
-}
+
+        //Bind Widget
+        bindWidget();
+
+    }   // Main Method
+
+    private void bindWidget() {
+
+        userEditText = (EditText) findViewById(R.id.editText3);
+        passwordEditText = (EditText) findViewById(R.id.editText4);
+        nameEditText = (EditText) findViewById(R.id.editText5);
+        surnameEditText = (EditText) findViewById(R.id.editText6);
+        phoneEditText = (EditText) findViewById(R.id.editText8);
+        emailEditText = (EditText) findViewById(R.id.editText9);
+        sexRadioGroup = (RadioGroup) findViewById(R.id.ragSex);
+
+
+    }   // bindWidget
+
+    public void clickSignUpSave(View view) {
+
+
+
+
+    }   // clickSignUpSave
+
+}   // Main Class
