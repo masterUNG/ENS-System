751a3edd93db3b55b0cf99647498e1f8c149683c
masterUNG <phrombutr@gmail.com>
2016-01-27 18:32:21 +0700

    Finial 27Jan16

--- app/src/main/java/appewtc/masterung/enssystem/ManageTABLE.java
+++ app/src/main/java/appewtc/masterung/enssystem/ManageTABLE.java
@@ -12,6 +12,28 @@ public class ManageTABLE {
     private MyOpenHelper objMyOpenHelper;
     private SQLiteDatabase writeSqLiteDatabase, readSqLiteDatabase;
 
+    public static final String TABLE_userTABLE = "userTABLE";
+    public static final String COLUMN_id = "_id";
+    public static final String COLUMN_User = "User";
+    public static final String COLUMN_Password = "Password";
+    public static final String COLUMN_Name = "Name";
+    public static final String COLUMN_Surname = "Surname";
+    public static final String COLUMN_Sex = "Sex";
+    public static final String COLUMN_Phone = "Phone";
+    public static final String COLUMN_Email = "Email";
+
+    public static final String TABLE_callTABLE = "callTABLE";
+    public static final String COLUMN_Name_Call = "Name_Call";
+    public static final String COLUMN_Phone_Call = "Phone_Call";
+
+    public static final String TABLE_newsTABLE = "newsTABLE";
+    public static final String COLUMN_Title_News = "Title_News";
+    public static final String COLUMN_Detail_News = "Detail_News";
+    public static final String COLUMN_Video_News = "Video_News";
+    public static final String COLUMN_Photo_News = "Photo_News";
+    public static final String COLUMN_Day_News = "Day_News";
+    public static final String COLUMN_User_Admin = "User_Admin";
+
     public ManageTABLE(Context context) {
 
         objMyOpenHelper = new MyOpenHelper(context);
