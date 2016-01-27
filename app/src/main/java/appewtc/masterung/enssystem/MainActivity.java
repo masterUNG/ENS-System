13256db9adf27d9c2ef425658f1ee3f69a953335
masterUNG <phrombutr@gmail.com>
2016-01-27 17:35:57 +0700

    Check Space Finish

--- app/src/main/java/appewtc/masterung/enssystem/MainActivity.java
+++ app/src/main/java/appewtc/masterung/enssystem/MainActivity.java
@@ -4,14 +4,51 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Explicit
+    private EditText userEditText, passwordEditText;
+    private String userString, passwordString;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Bind Widget
+        bindWidget();
+
     }   // Main Method
+
+    private void bindWidget() {
+
+        userEditText = (EditText) findViewById(R.id.editText);
+        passwordEditText = (EditText) findViewById(R.id.editText2);
+
+    }   // bindWidget
+
+    public void clickSignIn(View view) {
+
+        userString = userEditText.getText().toString().trim();
+        passwordString = passwordEditText.getText().toString().trim();
+
+        if (userString.equals("") || passwordString.equals("")) {
+            //Have Space
+            MyAlertDialog objMyAlertDialog = new MyAlertDialog();
+            objMyAlertDialog.MyDialog(MainActivity.this, R.drawable.icon_question,
+                    "Have Space", "Please Fill All Every Blank");
+
+        } else {
+            //No Space
+
+        }   // if
+
+
+    }   // clickSignIn
+
 
     public void clickSignUp(View view) {
         startActivity(new Intent(MainActivity.this, SignUpActivity.class));
