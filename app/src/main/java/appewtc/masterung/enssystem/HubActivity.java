e3c5da49e11bc620bbe276e1497dbf3333c2f5f1
masterUNG <phrombutr@gmail.com>
2016-02-04 17:05:52 +0700

    Finish 4Feb16

--- app/src/main/java/appewtc/masterung/enssystem/HubActivity.java
+++ app/src/main/java/appewtc/masterung/enssystem/HubActivity.java
@@ -1,13 +1,41 @@
 package appewtc.masterung.enssystem;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class HubActivity extends AppCompatActivity {
+
+    //Explicit
+    private TextView informTextView, newsTextView,
+            searchTextView, staticTextView, callTextView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hub);
+
+        //Bind Widget
+        bindWidget();
+
+    }   // Main Method
+
+    public void clickTHAI(View view) {
+
+    }   // clickTHAI
+
+    public void clickENG(View view) {
+
+    }   // clickENG
+
+    private void bindWidget() {
+        informTextView = (TextView) findViewById(R.id.textView9);
+        newsTextView = (TextView) findViewById(R.id.textView10);
+        searchTextView = (TextView) findViewById(R.id.textView11);
+        staticTextView = (TextView) findViewById(R.id.textView12);
+        callTextView = (TextView) findViewById(R.id.textView13);
     }
-}
+
+}   // Main Class
