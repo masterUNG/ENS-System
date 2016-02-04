9131f081fd373b66ad317b230f2a9d3e0bc69750
masterUNG <phrombutr@gmail.com>
2016-02-04 11:18:51 +0700

    add News finish

--- app/src/main/java/appewtc/masterung/enssystem/MainActivity.java
+++ app/src/main/java/appewtc/masterung/enssystem/MainActivity.java
@@ -1,6 +1,7 @@
 package appewtc.masterung.enssystem;
 
 import android.content.Intent;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -25,7 +26,50 @@ public class MainActivity extends AppCompatActivity {
         //Request Database
         objManageTABLE = new ManageTABLE(this);
 
+        //Test Add Value
+        testAddValue();
+
+        //Synchronize JSON to SQLite
+        synJSONtoSQLite();
+
+
     }   // Main Method
+
+    private void testAddValue() {
+
+        objManageTABLE.addUser("user", "pass", "name", "Sur", "male", "123456", "abc@gmail.com");
+
+        objManageTABLE.addNews("title", "detail", "video", "photo", "day news", "user admin");
+
+    }   // testAddValue
+
+    private void synJSONtoSQLite() {
+
+        //Change Policy
+        StrictMode.ThreadPolicy myPolicy = new StrictMode.ThreadPolicy
+                .Builder().permitAll().build();
+        StrictMode.setThreadPolicy(myPolicy);
+
+        int intTABLE = 1 ;   //intTABLE ==> amount of Table
+        while (intTABLE <= 2) {
+
+            String strURLuser = "http://swiftcodingthai.com/ens/php_get_user_master.php";
+            String strURLnews = "http://swiftcodingthai.com/ens/php_get_news_master.php";
+
+            //1. Create InputStream
+
+            //2. Create JSON String
+
+            //3. Update to SQLite
+
+
+
+            intTABLE += 1;
+        }   // while
+
+
+
+    }   // synJSONtoSQLite
 
     private void bindWidget() {
 
--- app/src/main/java/appewtc/masterung/enssystem/ManageTABLE.java
+++ app/src/main/java/appewtc/masterung/enssystem/ManageTABLE.java
@@ -1,5 +1,6 @@
 package appewtc.masterung.enssystem;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 
@@ -41,5 +42,45 @@ public class ManageTABLE {
         readSqLiteDatabase = objMyOpenHelper.getReadableDatabase();
 
     }   // Constructor
+
+    //Add New Value to newsTABLE
+    public long addNews(String strTitle_News,
+                        String strDetail_News,
+                        String strVideo_News,
+                        String strPhoto_News,
+                        String strDay_News,
+                        String strUser_Admin) {
+
+        ContentValues objContentValues = new ContentValues();
+        objContentValues.put(COLUMN_Title_News, strTitle_News);
+        objContentValues.put(COLUMN_Detail_News, strDetail_News);
+        objContentValues.put(COLUMN_Video_News, strVideo_News);
+        objContentValues.put(COLUMN_Photo_News, strPhoto_News);
+        objContentValues.put(COLUMN_Day_News, strDay_News);
+        objContentValues.put(COLUMN_User_Admin, strUser_Admin);
+
+        return writeSqLiteDatabase.insert(TABLE_newsTABLE, null, objContentValues);
+    }
+
+    //Add New Value to userTABLE
+    public long addUser(String strUser,
+                        String strPassword,
+                        String strName,
+                        String strSurname,
+                        String strSex,
+                        String strPhone,
+                        String strEmail) {
+
+        ContentValues objContentValues = new ContentValues();
+        objContentValues.put(COLUMN_User, strUser);
+        objContentValues.put(COLUMN_Password, strPassword);
+        objContentValues.put(COLUMN_Name, strName);
+        objContentValues.put(COLUMN_Surname, strSurname);
+        objContentValues.put(COLUMN_Sex, strSex);
+        objContentValues.put(COLUMN_Phone, strPhone);
+        objContentValues.put(COLUMN_Email, strEmail);
+
+        return writeSqLiteDatabase.insert(TABLE_userTABLE, null, objContentValues);
+    }
 
 }   // Main Class
