6afca41b4d541c41ec2f76da53ee64f25e871a6d
masterUNG <phrombutr@gmail.com>
2016-03-14 11:13:56 +0700

    Create Adapter Finish

--- app/src/main/java/appewtc/masterung/enssystem/NewsListView.java
+++ app/src/main/java/appewtc/masterung/enssystem/NewsListView.java
@@ -3,13 +3,25 @@ package appewtc.masterung.enssystem;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ListView;
 
 public class NewsListView extends AppCompatActivity {
+
+    //Explicit
+    private ListView newsListView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news_list_view);
+
+        //Bind Widget
+        newsListView = (ListView) findViewById(R.id.listView);
+
+
+
+
     } // Main Method
 
     public void clickBackNewsList(View view) {
