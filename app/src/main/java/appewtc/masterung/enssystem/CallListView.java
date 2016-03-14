4190b454c9d7fc9359140835300c7b4ddac8f0fa
masterUNG <phrombutr@gmail.com>
2016-03-14 15:20:56 +0700

    Design UI Call Finish

--- app/src/main/java/appewtc/masterung/enssystem/CallListView.java
+++ app/src/main/java/appewtc/masterung/enssystem/CallListView.java
@@ -2,12 +2,27 @@ package appewtc.masterung.enssystem;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class CallListView extends AppCompatActivity {
+
+    //Explicit
+    private ImageView cat0ImageView, cat1ImageView,
+            cat2ImageView,cat3ImageView, cat4ImageView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_call_list_view);
+
+        //Bind Widget
+        bindWidget();
+
+    }   // Main Method
+
+    private void bindWidget() {
+
     }
-}
+
+}   // Main Class
